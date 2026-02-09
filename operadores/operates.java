f3823e5cf8173121401e5fb3a15fdc346bd500b9
DavoDev-Hub <juanpajmz13@gmail.com>
2026-02-09 16:26:56 -0600

    Java Course

---
+++ operadores/operates.java
@@ -0,0 +1,22 @@
+package operadores;
+
+public class operates {
+    public static void main(String[] args) {
+        // Arithmetics +, -, *, /
+        // Relational ==, >, < >=, <=
+        // Conditional &&, ||, !
+        int num1, num2, resultArithmetics;
+        num1=3;
+        num2 = 2;
+        resultArithmetics = num1 + num2;
+        System.out.println(resultArithmetics);
+        resultArithmetics = num1 - num2;
+        System.out.println(resultArithmetics);
+        resultArithmetics = num1 * num2;
+        System.out.println(resultArithmetics);
+        resultArithmetics = num1 / num2;
+        System.out.println(resultArithmetics);
+        System.out.println(resultArithmetics);
+        System.out.println(resultArithmetics);
+    }
+}
---
+++ variables/variables.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class variables {
+    public static void main(String[] args) {
+        int numero = 5;
+        String nombre = "Pablo";
+        boolean flag = false;
+        char letra = 'l';
+        long largo = 123456789;
+
+        System.out.println(numero);
+        System.out.println(nombre);
+        System.out.println(flag);
+        System.out.println(letra);
+        System.out.println(largo);
+    }
+}
