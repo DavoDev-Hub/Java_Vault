abab1f4c5f5a4cdf4daea4d99b5c57145735ec9a
DavoDev-Hub <juanpajmz13@gmail.com>
2026-02-11 18:51:17 -0600

    arrays

---
+++ vectores_matrices/vectores.java
@@ -0,0 +1,15 @@
+package vectores_matrices;
+
+public class vectores {
+    public static void main(String[] args) {
+        int[] vector = new int [4];
+        vector[0] = 2;
+        vector[1] = 4;
+        vector[2] = 6;
+        vector[3] = 8;
+
+        for (int i = 0; i <vector.length; i++){
+            System.out.println(vector[i]);
+        }
+    }
+}
