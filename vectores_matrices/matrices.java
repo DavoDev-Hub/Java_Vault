5e204df6161a0fd0cfb09658bc22cacfb93b9cea
DavoDev-Hub <juanpajmz13@gmail.com>
2026-02-13 21:29:57 -0600

    matrices

--- vectores_matrices/matrices.java
+++ vectores_matrices/matrices.java
@@ -2,7 +2,7 @@ package vectores_matrices;
 
 public class matrices {
     public static void main(String[] args) {
-        int[][] matriz = new int[4][4];
+        int[][] matriz = new int[8][8];
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz.length; j++) {
                 matriz[i][j] = j;
