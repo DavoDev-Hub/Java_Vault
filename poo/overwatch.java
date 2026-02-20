a50c6b86246be4a603c916979937ee97a45ea60d
DavoDev-Hub <juanpajmz13@gmail.com>
2026-02-19 18:56:55 -0600

    poo

--- poo/overwatch.java
+++ poo/overwatch.java
@@ -1,4 +1,57 @@
 package poo;
 
 public class overwatch {
+    public static class Heroe {
+
+        private String nombre;
+        private int vida;
+        private int daño;
+
+        public Heroe(String nombre, int vida, int daño) {
+            this.nombre = nombre;
+            this.vida = vida;
+            this.daño = daño;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public int getVida() {
+            return vida;
+        }
+
+        public int getDaño() {
+            return daño;
+        }
+
+        public void atacar() {
+            System.out.println(nombre + " ataca causando " + daño + " de daño.");
+        }
+
+        public void habilidadEspecial() {
+            System.out.println(nombre + " usa una habilidad especial.");
+        }
+
+        public void mostrarInfo() {
+            System.out.println("Heroe: " + nombre);
+            System.out.println("Vida: " + vida);
+            System.out.println("Daño: " + daño);
+        }
+    }
+
+    public static class Tanque extends Heroe {
+
+        private int armadura;
+
+        public Tanque(String nombre, int vida, int daño, int armadura) {
+            super(nombre, vida, daño);
+            this.armadura = armadura;
+        }
+
+        @Override
+        public void habilidadEspecial() {
+            System.out.println(getNombre() + " activa escudo con " + armadura + " puntos de armadura.");
+        }
+    }
 }
