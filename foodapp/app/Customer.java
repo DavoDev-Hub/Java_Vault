267524f1fd7371689b7d9a0a1bcca7db055328ac
DavoDev-Hub <juanpajmz13@gmail.com>
2026-03-09 18:37:43 -0600

    Customer created

--- foodapp/app/Customer.java
+++ foodapp/app/Customer.java
@@ -1,8 +1,25 @@
 package foodapp.app;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class Customer {
+    private static int idCounter = 0;
+    private final int id;
+    private String name;
+    private List<Order> orders;
+
+    public Customer(String name){
+        this.id = ++idCounter;
+        this.name = name;
+        this.orders = new ArrayList<>();
+    }
+
+    public int getId(){ return id; }
+    public String getName(){ return name; }
+    public void addOrder(Order order){ orders.add(order); }
+    public List<Order> getOrders(){ return orders; }
+
 
 }
